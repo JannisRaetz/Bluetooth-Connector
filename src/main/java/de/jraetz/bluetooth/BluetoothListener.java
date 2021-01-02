74732767145197f9b40d4aa37adefae1a833633d
Jannis <jannisraetzspam@gmail.com>
2021-01-02 21:48:56 +0100

    Continued tutorial.

--- src/main/java/de/jraetz/bluetooth/BluetoothListener.java
+++ src/main/java/de/jraetz/bluetooth/BluetoothListener.java
@@ -1,9 +1,16 @@
 package de.jraetz.bluetooth;
 
+import java.io.OutputStream;
+import javax.bluetooth.DataElement;
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.DiscoveryListener;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
+import javax.microedition.io.Connector;
+import javax.obex.ClientSession;
+import javax.obex.HeaderSet;
+import javax.obex.Operation;
+import javax.obex.ResponseCodes;
 
 public class BluetoothListener implements DiscoveryListener {
 
@@ -35,10 +42,63 @@ public class BluetoothListener implements DiscoveryListener {
 
   @Override
   public void serviceSearchCompleted(int arg0, int arg1) {
+    synchronized (mLock) {
+      mLock.notify();
+    }
   }
 
   @Override
-  public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
+  public void servicesDiscovered(int arg0, ServiceRecord[] services) {
+    for (int i = 0; i < services.length; i++) {
+      String url = services[i].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+      if (url == null) {
+        continue;
+      }
+
+      DataElement serviceName = services[i].getAttributeValue(0x0100);
+      if (serviceName != null) {
+        System.out.println("service " + serviceName.getValue() + " found " + url);
+      } else {
+        System.out.println("service found " + url);
+      }
+
+      if (serviceName.getValue().equals("OBEX Object Push")) {
+        sendMessageToDevice(url);
+      }
+    }
+
+  }
+
+  private static void sendMessageToDevice(String serverURL) {
+    try {
+      System.out.println("Connecting to " + serverURL);
+
+      ClientSession clientSession = (ClientSession) Connector.open(serverURL);
+      HeaderSet hsConnectReply = clientSession.connect(null);
+      if (hsConnectReply.getResponseCode() != ResponseCodes.OBEX_HTTP_OK) {
+        System.out.println("Failed to connect");
+        return;
+      }
+
+      HeaderSet hsOperation = clientSession.createHeaderSet();
+      hsOperation.setHeader(HeaderSet.NAME, "Hello.txt");
+      hsOperation.setHeader(HeaderSet.TYPE, "text");
+
+      //Create PUT Operation
+      Operation putOperation = clientSession.put(hsOperation);
+
+      // Sending the message
+      byte data[] = "Hello World !!!".getBytes("iso-8859-1");
+      OutputStream os = putOperation.openOutputStream();
+      os.write(data);
+      os.close();
+
+      putOperation.close();
+      clientSession.disconnect(null);
+      clientSession.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
--- src/main/java/de/jraetz/bluetooth/BluetoothServer.java
+++ src/main/java/de/jraetz/bluetooth/BluetoothServer.java
@@ -1,13 +1,20 @@
 package de.jraetz.bluetooth;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.UUID;
 
 public class BluetoothServer {
 
   private static Object lock = new Object();
 
   public static void main(String[] args) {
+
+    ArrayList<RemoteDevice> devices = new ArrayList<>();
 
     try {
       // 1
@@ -28,8 +35,32 @@ public class BluetoothServer {
       }
       System.out.println("Device Inquiry Completed. ");
 
+      devices.addAll(Arrays.asList(agent.retrieveDevices(0)));
+
     } catch (Exception e) {
       e.printStackTrace();
+    }
+
+    UUID[] uuidSet = new UUID[1];
+    uuidSet[0] = new UUID(0x1105); //OBEX Object Push service
+
+    int[] attrIDs = new int[]{
+        0x0100 // Service name
+    };
+
+    try {
+      LocalDevice localDevice = LocalDevice.getLocalDevice();
+      DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+      for (RemoteDevice device : devices) {
+        agent.searchServices(null, uuidSet, device, new BluetoothListener(lock));
+      }
+
+      synchronized (lock) {
+        lock.wait();
+      }
+    } catch (InterruptedException | BluetoothStateException e) {
+      e.printStackTrace();
+      return;
     }
   }
 
